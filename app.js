92917b90c8be20e4d5befd862b63e59a1ffab4c3
sudhanshu uniyal <uniyalsudhanshu10@gmail.com>
2023-02-25 22:33:43 +0530

    setup for playo app

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+const router = require("./router")
+
+app.use("/", router)
+
+module.exports = app
---
+++ db.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require("mongodb")
+const dotenv = require("dotenv")
+dotenv.config()
+const client = new MongoClient(process.env.CONNECTIONSTRING)
+
+async function start() {
+  await client.connect()
+  const app = require("./app")
+  app.listen(process.env.PORT)
+}
+start()
